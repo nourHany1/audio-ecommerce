2ee33d2f37bfc69d2d6bf93068a38b9f5d782443
nourHany1 <162379890+nourHany1@users.noreply.github.com>
2024-12-24 22:44:11 +0200

    commit 10

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // Other configurations...
 };
 
-export default nextConfig;
+module.exports = nextConfig;
