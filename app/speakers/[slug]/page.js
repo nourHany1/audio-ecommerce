f8b8b3dc0c16f8948fd540cdd73def5842250b7a
nourHany1 <162379890+nourHany1@users.noreply.github.com>
2025-07-24 22:08:15 +0300

    feat: first commit

--- app/speakers/[slug]/page.js
+++ app/speakers/[slug]/page.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import data from '../../../data.json'
 import ProductDetails from '@/app/components/ProductDetails/ProductDetails'
-const ProductDetailPage = ({params}) => {
-  const product = data.filter((p) => p.slug === params.slug)
+const ProductDetailPage = async ({ params }) => {
+  const product = data.filter((p) => p.slug === params.slug);
   return (
     <div>
-      <ProductDetails products = {product}/>
+      <ProductDetails products={product} />
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetailPage
